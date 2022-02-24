c2d5924b245598192afcea276d0c57d8e6c362bb
Abhishek Kumar Singh <88192065+abhishekit04@users.noreply.github.com>
2022-02-24 19:24:49 +0530

    Update string.cs

--- string.cs
+++ string.cs
@@ -1,4 +1,22 @@
-string Greeting="Hello";
-int len = Greeting.Length;
-Console.WriteLine(Greeting + " has "+ len + " charecters");
-Console.WriteLine($"{Greeting} has {len} charecters");
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string Greeting="Hello";
+      int len = Greeting.Length;
+      Console.WriteLine(Greeting + " has "+ len + " charecters");
+      Console.WriteLine($"{Greeting} has {len} charecters");
+      string upper = Greeting.ToUpper();
+      string lower = Greeting.ToLower();
+      Console.WriteLine($"{upper}");
+      Console.WriteLine($"{lower}");
+      string output = lower+upper+Greeting;
+      Console.WriteLine($"{output}");
+      
+     }
+  }
+}
